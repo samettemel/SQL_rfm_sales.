936d4484d1ce7113e6603dd2defae26ea98ba08a
Samet <123364622+samettemel@users.noreply.github.com>
2024-08-19 10:41:30 +0300

    Update project.sql

--- project.sql
+++ project.sql
@@ -3,23 +3,23 @@ select  * from sales_data_sample
 
 --Checing unique values
 
-select distinct STATUS from sales_data_sample--Nice oneto plot
+select distinct STATUS from sales_data_sample--
 select distinct YEAR_ID from sales_data_sample
-select distinct  PRODUCTLINE from sales_data_sample--Nice to plot
-select distinct COUNTRY from sales_data_sample--Nice to plot
-select distinct DEALSIZE from sales_data_sample--Nice to plot
-select distinct TERRITORY from sales_data_sample--Nice to plot
+select distinct  PRODUCTLINE from sales_data_sample--
+select distinct COUNTRY from sales_data_sample--
+select distinct DEALSIZE from sales_data_sample
+select distinct TERRITORY from sales_data_sample--
 
 
 --Analysis 
 --start by grouping sales by productline
 
-select PRODUCTLINE ,sum(SALES) Revenue -->Hasýlat demektir
+select PRODUCTLINE ,sum(SALES) Revenue -->Hasilat demektir
 from sales_data_sample 
 group by PRODUCTLINE
 order by 2 desc
 
-select YEAR_ID ,sum(SALES) Revenue -->Hasýlat demektir
+select YEAR_ID ,sum(SALES) Revenue
 from sales_data_sample 
 group by YEAR_ID
 order by 2 desc
@@ -27,7 +27,7 @@ order by 2 desc
 select distinct MONTH_ID from sales_data_sample
 where YEAR_ID=2005
 
-select DEALSIZE ,sum(SALES) Revenue -->Hasýlat demektir
+select DEALSIZE ,sum(SALES) Revenue 
 from sales_data_sample 
 group by DEALSIZE
 order by 2 desc
@@ -86,7 +86,7 @@ FROM
 	select CUSTOMERNAME , rfm_recency, rfm_frequency, rfm_monetary,
 	case 
 		when rfm_cellstring in (111, 112 , 121, 122, 123, 132, 211, 212, 114, 141) then 'lost_customers'  --lost customers
-		when rfm_cellstring in (133, 134, 143, 244, 334, 343, 344, 144) then 'slipping away, cannot lose' -- (Big spenders who haven’t purchased lately) slipping away
+		when rfm_cellstring in (133, 134, 143, 244, 334, 343, 344, 144) then 'slipping away, cannot lose' -- (Big spenders who havenâ€™t purchased lately) slipping away
 		when rfm_cellstring in (311, 411, 331) then 'new customers'
 		when rfm_cellstring in (222, 223, 233, 322) then 'potential churners'
 		when rfm_cellstring in (323, 333,321, 422, 332, 432) then 'active' --(Customers who buy often & recently, but at low price points)
